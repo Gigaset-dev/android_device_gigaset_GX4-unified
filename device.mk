3334f622ec0342cca99908e229941d710091c10a
LuK1337 <priv.luk@gmail.com>
2022-12-13 19:13:56 +0000

    GX4: Build libhidltransport/libhwbinder * Needed for old blobs
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- device.mk
+++ device.mk
@@ -45,6 +45,13 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     fastbootd
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 31
 
