972a056d375f6306fafb2e8543f02124248199c8
Erfan Abdi <erfangplus@gmail.com>
2023-03-06 17:28:37 +0330

    GX4: Build bootctl hal
    Change-Id: I12646e586c44f3c4c571d6659766fb76d94f8469

--- device.mk
+++ device.mk
@@ -28,6 +28,15 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery \
+    android.hardware.boot@1.2-service
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
