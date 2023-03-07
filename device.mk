0be8ae1d31dfb9d01afde30dc036c9f6a3451013
Erfan Abdi <erfangplus@gmail.com>
2023-03-07 15:40:16 +0330

    GX4: Import recovery init script
    Change-Id: If412f0956b7a699549563d2b7e4a857afe46101f

--- device.mk
+++ device.mk
@@ -312,6 +312,7 @@ PRODUCT_PACKAGES += \
     init.mt6789.usb.rc \
     init.mtkgki.rc \
     init.project.rc \
+    init.recovery.mt6789.rc \
     init.sensor_2_0.rc \
     init.stnfc.rc \
     ueventd.mtk.rc
