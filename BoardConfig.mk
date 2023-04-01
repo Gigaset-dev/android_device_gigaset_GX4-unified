3ca60443ad3f1e992935bc124acdd8128fefd895
Erfan Abdi <erfangplus@gmail.com>
2023-04-01 15:23:22 +0330

    GX4: Address missing sepolicies
    Change-Id: Ie5402129c7e0209974eb19d1b5bed52460de1913

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,6 +136,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # SPL
 VENDOR_SECURITY_PATCH := 2022-11-01
