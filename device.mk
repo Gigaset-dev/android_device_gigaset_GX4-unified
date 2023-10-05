dd6b3e86c1edfc56bc2d1394082f98587ab04bd5
bengris32 <bengris32@protonmail.ch>
2023-10-05 21:29:06 +0530

    GX4: Switch to AOSP hostapd service
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I1f634de16e59f194784ac5f2863c1a00de321876
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- device.mk
+++ device.mk
@@ -448,8 +448,8 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
+    hostapd \
     android.hardware.wifi@1.5.vendor \
-    android.hardware.wifi.hostapd@1.3.vendor \
     android.hardware.wifi.supplicant@1.4.vendor \
     android.hardware.tetheroffload.config@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor
