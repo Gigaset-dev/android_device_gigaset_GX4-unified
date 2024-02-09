f73275e1c0ddf6b4d6640e597cab1b726fb36687
Erfan Abdi <erfangplus@gmail.com>
2024-02-09 16:22:32 +0330

    GX4: InCallService: Use STREAM_VOICE_CALL for volumeIndex
    Change-Id: Ia50e28c1a4d8273d991ae613f057509bfbb9699e

--- app/InCallService/src/org/lineageos/mediatek/incallservice/VolumeChangeReceiver.java
+++ app/InCallService/src/org/lineageos/mediatek/incallservice/VolumeChangeReceiver.java
@@ -23,12 +23,9 @@ public class VolumeChangeReceiver extends BroadcastReceiver {
         if (intent.getIntExtra(AudioManager.EXTRA_VOLUME_STREAM_TYPE, -1) == AudioManager.STREAM_VOICE_CALL) {
             AudioDeviceInfo callDevice = mAudioManager.getCommunicationDevice();
 
-            // Try to get volumeIndex
             int volumeIndex = intent.getIntExtra(AudioManager.EXTRA_VOLUME_STREAM_VALUE, -1);
-            if (volumeIndex < 0) {
-                Log.w(LOG_TAG, "Could not get volumeIndex!");
-                return;
-            }
+            if (volumeIndex == -1)
+                volumeIndex = mAudioManager.getStreamVolume(AudioManager.STREAM_VOICE_CALL);
 
             GainUtils.setGainLevel(callDevice.getPort().type(), volumeIndex, AudioSystem.STREAM_VOICE_CALL);
         }
