78b4d569e00e2f0473aff6ce3401c39f3d02b549
Erfan Abdi <erfangplus@gmail.com>
2024-02-09 16:20:01 +0330

    Revert "GX4: InCallService: Map AOSP gain step range to MTK one"
    This reverts commit 56d9e5ed90345c09c4b4494d4e9663e9df105bcd.
    
    Change-Id: I73a5e2ac1847a182c8268d18f7637ce57c9fc120

--- app/InCallService/src/org/lineageos/mediatek/incallservice/GainUtils.java
+++ app/InCallService/src/org/lineageos/mediatek/incallservice/GainUtils.java
@@ -7,15 +7,17 @@ import android.util.Log;
 
 public class GainUtils {
     public static final String LOG_TAG = "MediatekInCallService";
+    public static final int volSteps = SystemProperties.getInt("ro.config.vc_call_vol_steps", 7);
 
+    /**
+     * Sets the gain level for a given audio device.
+     * @param audioDevice The audio device to set the gain level for.
+     * @param gainIndex The gain level to set.
+     * @param streamType The stream type to set the gain level for.
+     */
     public static void setGainLevel(int audioDevice, int gainIndex, int streamType) {
-        int maxStep = SystemProperties.getInt("ro.config.vc_call_vol_steps", 7);
         String parameters = String.format("volumeDevice=%d;volumeIndex=%d;volumeStreamType=%d",
-                                          audioDevice, 
-                                          Math.round(
-                                            (15.0 / Math.log(maxStep + 1.0))
-                                            * Math.log(Math.min(maxStep, gainIndex) + 1.0)),
-                                          streamType);
+                                          audioDevice, Math.min(volSteps, gainIndex), streamType);
         Log.d(LOG_TAG, "Setting audio parameters to: " + parameters);
         AudioSystem.setParameters(parameters);
     }
