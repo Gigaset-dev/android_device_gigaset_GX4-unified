f16f428f625d6c4a0faf8e8f6bd69fda49ac48a0
Erfan Abdi <erfangplus@gmail.com>
2024-02-09 16:14:31 +0330

    GX4: Add notifications access dialog
    Change-Id: I909d60c566249cfeaf3d7a5f3c90a9cccdad4f04

--- Spotlight/src/com/volla/spotlight/Settings/SettingsFragment.java
+++ Spotlight/src/com/volla/spotlight/Settings/SettingsFragment.java
@@ -18,7 +18,10 @@
 
 package com.volla.spotlight.Settings;
 
+import android.content.ComponentName;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
 import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Bundle;
@@ -26,6 +29,7 @@ import android.os.Handler;
 import android.provider.Settings;
 import android.widget.Switch;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragment;
@@ -40,10 +44,14 @@ import com.volla.spotlight.Manager.LEDManager;
 import com.volla.spotlight.R;
 import com.volla.spotlight.Constants.Constants;
 import com.volla.spotlight.Manager.SettingsManager;
+import com.volla.spotlight.Services.NotificationService;
 import com.volla.spotlight.Utils.ServiceUtils;
 
 public class SettingsFragment extends PreferenceFragment implements OnPreferenceChangeListener,
         OnMainSwitchChangeListener {
+
+    /** Hidden field Settings.Secure.ENABLED_NOTIFICATION_LISTENERS */
+    private static final String NOTIFICATION_ENABLED_LISTENERS = "enabled_notification_listeners";
 
     private MainSwitchPreference mSwitchBar;
 
@@ -62,6 +70,9 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.spotlight_settings);
+
+        if (!isNotifListenerEnabled())
+            SettingsManager.setSpotlightNotifsEnabled(getActivity(), false);
 
         mContentResolver = getActivity().getContentResolver();
         mSettingObserver = new SettingObserver();
@@ -102,12 +113,51 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
         mMusicPreference.setOnPreferenceChangeListener(this);
     }
 
+    private boolean isNotifListenerEnabled() {
+        boolean serviceEnabled = true;
+        String enabledListeners = Settings.Secure.getString(
+                getContext().getContentResolver(), NOTIFICATION_ENABLED_LISTENERS);
+        ComponentName myListener =
+                new ComponentName(getContext(), NotificationService.class);
+        serviceEnabled = enabledListeners != null &&
+                (enabledListeners.contains(myListener.flattenToString()) ||
+                        enabledListeners.contains(myListener.flattenToShortString()));
+
+        return serviceEnabled;
+    }
+
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final String preferenceKey = preference.getKey();
 
         if (preferenceKey.equals(Constants.SPOTLIGHT_NOTIFS_ENABLE)) {
-            SettingsManager.setSpotlightNotifsEnabled(getActivity(), true);
+            if ((boolean) newValue) {
+                if (isNotifListenerEnabled()) {
+                    SettingsManager.setSpotlightNotifsEnabled(getActivity(), true);
+
+                    Intent intent = new Intent(getActivity(), NotifsSettingsActivity.class);
+                    getActivity().startActivity(intent);
+                } else {
+                    SettingsManager.setSpotlightNotifsEnabled(getActivity(), false);
+                    mNotifsPreference.setChecked(false);
+
+                    // Show a dialog to the user
+                    final Context context = getActivity();
+                    String msg = context.getString(R.string.msg_missing_notification_access,
+                            context.getString(R.string.spotlight_settings_app_name));
+                    new AlertDialog.Builder(context)
+                            .setTitle(R.string.title_missing_notification_access)
+                            .setMessage(msg)
+                            .setNegativeButton(android.R.string.cancel, null)
+                            .setPositiveButton(R.string.title_change_settings, (dialog, which) -> {
+                                Intent intent = new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)
+                                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                getActivity().startActivity(intent);
+                            }).create().show();
+                }
+            } else {
+                SettingsManager.setSpotlightNotifsEnabled(getActivity(), false);
+            }
         }
 
         if (preferenceKey.equals(Constants.SPOTLIGHT_BRIGHTNESS)) {
