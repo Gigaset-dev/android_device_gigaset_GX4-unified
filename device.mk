52748dd8ed6565a44b96c8b3199d67116e69d7ce
alextrack2013 <alextrack2013@gmail.com>
2024-02-14 12:27:48 +0100

    GX4: Switch to AOSP AIDL Boot HAL
    Change-Id: I74698c395a7a5c64bdc44d2a99a251eeb5a3519e
    Signed-off-by: alextrack2013 <alextrack2013@gmail.com>

--- device.mk
+++ device.mk
@@ -94,9 +94,8 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot-service.default \
+    android.hardware.boot-service.default_recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
