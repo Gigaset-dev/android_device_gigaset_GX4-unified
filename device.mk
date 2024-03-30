3f865376b2ef9a0eb9757742118f5ae795a606a4
electimon <electimon@gmail.com>
2024-03-30 03:53:56 +0000

    GX4: Build wpa_supplicant from source
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: I377144da17eb86e3b8a370afebafd11285b628e3

--- device.mk
+++ device.mk
@@ -449,7 +449,8 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi.supplicant@1.4.vendor \
     android.hardware.tetheroffload.config@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor \
-    libwifi-hal-wrapper
+    libwifi-hal-wrapper \
+    wpa_supplicant
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
