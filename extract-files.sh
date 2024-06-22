70edb4c3302f3b370b26d420081a1fc938b9eede
ChrisCatto <chriscatto08@gmail.com>
2024-06-22 16:29:50 +0200

    GX4: Link mediatek camera provider against v32 `libutils`
    * Abort message: 'incStrongRequireStrong() called on 0xb40000770d0587a0
      which isn't already owned'
    * Required after U QPR3
    
    Change-Id: Ie6ea7f331207f604cb87804c67f4de9fd9a60d76
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -78,7 +78,8 @@ function blob_fixup {
         vendor/lib*/hw/vendor.mediatek.hardware.pq@2.15-impl.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
-        vendor/lib64/libmtkcam_stdutils.so)
+        vendor/lib64/libmtkcam_stdutils.so|\
+        vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
         vendor/etc/gnss/agps_profiles_conf2.xml)
