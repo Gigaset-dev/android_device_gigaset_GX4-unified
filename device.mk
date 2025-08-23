6d7c8930411f2427502e7defdc7fa1e16421ca14
techyminati <sinha.aryan03@gmail.com>
2025-08-23 17:58:02 +0530

    GX4: Migrate TARGET_INIT_VENDOR_LIB to soong_config_set,libinit,vendor_init_lib
    * Fixes warning :-
      vendor/lineage/config/BoardConfigSoong.mk:99: warning: TARGET_INIT_VENDOR_LIB is deprecated, please migrate to soong_config_set,libinit,vendor_init_lib
    
    Change-Id: Ib432c795cb94db921721a211412f63d95d9745c2
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- device.mk
+++ device.mk
@@ -257,6 +257,9 @@ $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 
+# Init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_gigaset_gx4)
+
 # Media
 PRODUCT_PACKAGES += \
     libcodec2_hidl@1.2.vendor \
